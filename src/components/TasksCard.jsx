7c23ea99a0e08ee866edc1b4abe92e0e5d7ee457
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-15 01:12:32 -0300

    detail

--- src/components/TasksCard.jsx
+++ src/components/TasksCard.jsx
@@ -1,7 +1,7 @@
 export default function TasksCard(){
     return(
       <div className="w-1/2 h-1/2">
-        <h1>Hola</h1>
+        <h1>Task Tracker</h1>
         <input className="w-full bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Start writing and press enter to create task" />  
       </div>
     );
