1acf248d563cca3e2bef6c4a52f0a9a2a6eaef6d
Marco D' Antino <marco.dantino@colidevs.com>
2026-04-16 13:03:54 -0300

    liquid-button-glass

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LiquidGlass from 'liquid-glass-react'
 
 function Task({ title = "Task Title" }: { title: string }) {
 
@@ -19,9 +20,23 @@ function AddTask({ title, handleTitle }: AddTaskProps){
   return(
     <div className="flex items-center px-4 border border-slate-200 rounded-full transition focus:shadow focus:border-slate-400 duration-300 ease hover:border-slate-300 shadow-md w-[90%]">
       <input id="input_Task" value={title} onChange={handleTitle} className="w-full rounded-full h-14 focus:outline-none bg-white placeholder:text-slate-400 text-slate-700 text-xl pl-2 pb-1" placeholder="Start writing and press enter to create task"/>
-      <button type="button" className="bg-slate-200 px-3 rounded-full text-sm h-fit py-2">
+      {/* <button type="button" className="bg-slate-200 px-3 rounded-full text-sm h-fit py-2">
         +
-      </button>
+      </button> */}
+      <LiquidGlass
+        blurAmount={0.1}
+        displacementScale={34}
+        saturation={3}
+        aberrationIntensity={13}
+        elasticity={0.39}
+        cornerRadius={100}
+        padding="12px 23px"
+        className="hover:cursor-pointer"
+        style={{ position: 'fixed', top: '45%', left: '62%' }}
+        onClick={() => console.log('Button clicked!')}
+      >
+        <span className="text-slate-800 font-medium">Add</span>
+      </LiquidGlass>
     </div>
   )
 }
